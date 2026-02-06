2f34685a1527139f899cadc5c854ee5c30cbab72
11172029 <vaniaafrin28@gmail.com>
2026-02-06 14:27:05 +0700

    Implement JWT Aut (Register & Login)

---
+++ app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use  Illuminate\Support\Facades\Validator;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+          $validator = Validator::make($request->all(), [
+            
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors());
+        }
+
+        $credentials = $request->only('email', 'password');
+       
+        if(!$token = auth()->guard('api')->attempt($credentials)){
+            return response()->json([
+                'success' => false,
+                'mesaage' => 'User atau Password Salah'
+            ], 401);
+        };
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Login Berhasil',
+            'user' => auth()->guard('api')->user(),
+            'token' => $token
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\MovieController;
+use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\RegisterController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::apiResource('movies', MovieController::class);
+Route::post('/register', RegisterController::class);
+Route::post('/login', LoginController::class);
